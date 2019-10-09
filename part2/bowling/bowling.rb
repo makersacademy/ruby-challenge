4fa770598611c3b5c251b504d1237332d3a91eca
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-09 14:32:00 +0100

    points after strikes counted

--- part2/bowling/bowling.rb
+++ part2/bowling/bowling.rb
@@ -9,14 +9,24 @@ class Game
         @rolls << pins
     end
 
-    def score()
-       
+    def spare_check
         for i in (0...@rolls.size-3).step(2) do
             if @rolls[i] + @rolls[i+1] == 10
                 @rolls[i+1] += @rolls[i+2] 
             i += 2
             end 
         end
+    end
+
+    def strike_check
+        if @rolls[0] == 10
+            @rolls[0] += @rolls[1] + @rolls[2]
+        end
+    end
+
+    def score
+        spare_check
+        strike_check
         @score = @rolls.sum
     end
 
