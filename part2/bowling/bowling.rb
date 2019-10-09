210a2ad81834076a30560d30bc9719234b5859cb
Rob Baile <robbaile12@outlook.com>
2019-10-09 11:50:42 +0100

    got test 3 & 4 to pass

--- part2/bowling/bowling.rb
+++ part2/bowling/bowling.rb
@@ -9,7 +9,43 @@ class Game
   end
 
   def score
-    @score = @rolls.reduce(:+) 
+    add_total_score
     @score
+  end
+
+  def add_total_score 
+    @score = score_each_go.reduce(:+)
+  end
+  
+  def score_each_go
+    each_go_score = []
+    current_go = 0 
+    until current_go === split_rolls.length
+      if is_a_strike?(split_rolls[current_go])
+        total_go_score = split_rolls[current_go].reduce(:+) + split_rolls[current_go + 1].reduce(:+)
+        each_go_score << total_go_score
+      elsif is_a_spare?(split_rolls[current_go])
+        total_go_score = split_rolls[current_go].reduce(:+) + split_rolls[current_go + 1].first
+        each_go_score << total_go_score
+      else
+        total_go_score = split_rolls[current_go].reduce(:+)
+        each_go_score << total_go_score
+      end
+      current_go += 1
+    end
+    return each_go_score
+  end
+
+  def is_a_spare?(go)
+    go.reduce(:+) === 10    
+  end
+
+  def is_a_strike?(go)
+    go.first === 10 
+  end
+
+
+  def split_rolls
+    @rolls.each_slice(2).to_a
   end
 end
--- part2/bowling/bowling_test.rb
+++ part2/bowling/bowling_test.rb
@@ -28,7 +28,7 @@ class BowlingTest < Minitest::Test
   end
 
   def test_points_scored_in_the_roll_after_a_spare_are_counted_twice
-    skip
+    # skip
     game = Game.new
     rolls = [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     rolls.each { |pins| game.roll(pins) }
@@ -36,7 +36,7 @@ class BowlingTest < Minitest::Test
   end
 
   def test_consecutive_spares_each_get_a_one_roll_bonus
-    skip
+    # skip
     game = Game.new
     rolls = [5, 5, 3, 7, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     rolls.each { |pins| game.roll(pins) }
