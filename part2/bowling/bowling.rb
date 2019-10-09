ea67e593f2d257871eb1719d3c36bc06b1fde74c
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-09 11:58:44 +0100

    points after spares counted twice

--- part2/bowling/bowling.rb
+++ part2/bowling/bowling.rb
@@ -1,15 +1,19 @@
 class Game
 
     def initialize
+        @rolls = []
         @score = 0
     end
 
     def roll(pins)
-        @score += pins
+        @rolls << pins
     end
 
-    def score
-        @score
+    def score()
+        if @rolls[0] + @rolls[1] == 10
+            @rolls[2] = @rolls[2]*2
+        end
+        @score = @rolls.sum
     end
 
 end
